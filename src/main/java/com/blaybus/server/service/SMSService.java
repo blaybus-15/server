e60e6a8650ab8f3c18a7e47225ec18774446957e
j30ngwoo <ttangle1234@naver.com>
2025-02-13 13:17:38 +0900

    sms 전송 테스트용 컨트롤러 추가

--- src/main/java/com/blaybus/server/service/SMSService.java
+++ src/main/java/com/blaybus/server/service/SMSService.java
@@ -3,7 +3,6 @@ package com.blaybus.server.service;
 import com.blaybus.server.config.SMS.SMSProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.nurigo.sdk.NurigoApp;
 import net.nurigo.sdk.message.model.Message;
 import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
 import net.nurigo.sdk.message.response.SingleMessageSentResponse;
