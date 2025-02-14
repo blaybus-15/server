bbd0e0d3eee234d4e07950aa21db12e58ad279db
Hyohyoju <julie08051@naver.com>
2025-02-14 09:54:46 +0900

    Reapply "Feat: KakoLogin #1"
    This reverts commit f34ea0958bf437cb218139afdc099878e4961ed7.

---
+++ src/main/java/com/blaybus/server/user/OAuth2UserInfo.java
@@ -0,0 +1,28 @@
+package com.blaybus.server.user;
+
+import com.blaybus.server.user.OAuth2Provider;
+
+import java.util.Map;
+
+public interface OAuth2UserInfo {
+
+    OAuth2Provider getProvider();
+
+    String getAccessToken();
+
+    Map<String, Object> getAttributes();
+
+    String getId();
+
+    String getEmail();
+
+    String getName();
+
+    String getFirstName();
+
+    String getLastName();
+
+    String getNickname();
+
+    String getProfileImageUrl();
+}
